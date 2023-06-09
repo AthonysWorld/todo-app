5d8b70c4f86dcbd255c75a0a46d2fb0e586991f8
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-06-09 20:00:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 
+
+
+
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-
-    if(inputBox.value=="") {  
-alert("plans can not be blank")
-} else{
-
+    if (inputBox.value == "") {
+        alert("plans cannot be blank!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData()
 }
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
 }
-
-
-// let li = document.createElement("li");
-// li.innerHtml = inputBox.value;
-// listContainer.appendChild(li);
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
